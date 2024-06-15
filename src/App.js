e1ac39c91d4b66742608609d0e793058c83c40fa
disami <samsonmamuye360@gmail.com>
2024-06-15 09:12:44 -0100

    react commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const element = <h1>Hello, world!</h1>;
+function App() {
+  return (
+    <div>
+      <main>
+      {element}
+      </main>
+    </div>
+  );
+}
+export default App;
+
